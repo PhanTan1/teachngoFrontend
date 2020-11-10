9c742b71d4892b0f17dc68625358e5ba7eb80db3
ffirouch <fouad.firouchane@gmail.com>
2020-11-10 20:16:53 +0900

    add footer and navigation bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
-import TeacherComponent from './components/TeacherComponent'
+import styled from "styled-components"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Teachers from "./pages/Teachers"
 import Login from "./pages/Login"
+import Navigation from "./components/Navigation"
+import Footer from './components/Footer'
+
+
+const Wrapper = styled.div`
+  min-height: calc(100vh - 70px);
+`
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          {/* <Route /> */}
           <Route path="/login" render={props => <Login {...props} />} />
-          <Route path="/teachers" render={props => <TeacherComponent {...props} />} />
+          <>
+            <Navigation />
+            <Wrapper>
+              {/* <Route /> */}
+              <Route path="/teachers" render={props => <Teachers {...props} />} />
+            </Wrapper>
+            <Footer />
+          </>
         </Switch>
       </Router>
     </div>

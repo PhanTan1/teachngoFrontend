da91aaf43f63424e8357fa5d59512cbff28999ed
ffirouch <fouad.firouchane@gmail.com>
2020-11-10 19:25:42 +0900

    ajout des routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import TeacherComponent from './components/TeacherComponent'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./pages/Login"
 
 function App() {
   return (
     <div className="App">
-        <TeacherComponent />
+      <Router>
+        <Switch>
+          {/* <Route /> */}
+          <Route path="/login" render={props => <Login {...props} />} />
+          <Route path="/teachers" render={props => <TeacherComponent {...props} />} />
+        </Switch>
+      </Router>
     </div>
   );
 }

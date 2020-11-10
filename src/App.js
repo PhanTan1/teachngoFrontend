4bdb4815a10945ea5e56528cfbf93436cf908290
ffirouch <fouad.firouchane@gmail.com>
2020-11-10 20:21:16 +0900

    add Home Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import styled from "styled-components"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Teachers from "./pages/Teachers"
+import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Navigation from "./components/Navigation"
 import Footer from './components/Footer'
@@ -20,7 +21,7 @@ function App() {
           <>
             <Navigation />
             <Wrapper>
-              {/* <Route /> */}
+              <Route path="/" exact render={props => <Home {...props} />} />
               <Route path="/teachers" render={props => <Teachers {...props} />} />
             </Wrapper>
             <Footer />

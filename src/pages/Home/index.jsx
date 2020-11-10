121371d51547b5cb8edd1a82d0965b0beb1b2c1b
ffirouch <fouad.firouchane@gmail.com>
2020-11-10 20:21:34 +0900

    add Home  page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Home = (props) => {
+  return (
+    <>
+      <h1>Welcome to teach and go</h1>
+      <p>
+        This page is under construction.
+        Please visit us again soo and find the teacher thats suits your requirements the best
+    </p>
+    </>
+  )
+}
+
+export default Home

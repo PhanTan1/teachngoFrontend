4eebbcf818d85ba4acb5c30076a37a63577aa336
ffirouch <fouad.firouchane@gmail.com>
2020-11-11 18:04:38 +0900

    update Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.span`
-    border: 2px solid black;
+    border: 2px solid #2A9D8F;
     padding: ${props => props.size === 'xs' ? '5px 0px' : '10px 40px'};
     ${props => props.size === 'xs' ? 'font-size: 12px' : null};
     border-radius: 5px;
     font-weight: bold;
     transition: all 0.2s ease-in-out;
     text-align: center;
-    color: black;
+    color: #2A9D8F;
     cursor: pointer;
 
     &:hover {
-        background-color: green;
-        
+        background-color: #2A9D8F;
+        color: white;
     }
 `
 

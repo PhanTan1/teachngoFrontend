0e7dd26c94356ddbbe687883d870ad29da988a82
ffirouch <fouad.firouchane@gmail.com>
2020-11-17 17:12:04 +0900

    refact Navigation comp

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import Button from "../Button"
 
 const Nav = styled.nav`
   display: flex;
   justify-content: flex-end;
   width: 100%;
   padding: 10px 30px;
-  border-bottom: 1px solid black;
   min-height: 50px;
+  align-items: center;
+  box-shadow: 0px 0px 20px -10px #000000
 `
 
 const List = styled.ul`
   list-style: none;
   display: flex;
-  max-width: 300px;
+  max-width: 400px;
   justify-content: space-between;
   width: 100%;
   margin-bottom: 0px;
+  margin-right: 20px;
 `
 
 const StyledLink = styled(Link)`
@@ -40,6 +43,7 @@ const Navigation = (props) => {
         <StyledLink to="/students"><li>Students</li></StyledLink>
         <StyledLink to="/login"><li>Login</li></StyledLink>
       </List>
+      <Button color="primary">Appelez nous</Button>
     </Nav>
   )
 }

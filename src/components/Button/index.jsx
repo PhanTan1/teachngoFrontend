531e4e98cc80a390725779a6f6c0b8b96feaf13e
ffirouch <fouad.firouchane@gmail.com>
2020-11-17 17:10:54 +0900

    refactored button comp

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,29 +1,39 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import {
+  createStyles,
+  makeStyles,
+  Theme,
+  ThemeProvider,
+  createMuiTheme
+} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const Wrapper = styled.span`
-    border: 2px solid #2A9D8F;
-    padding: ${props => props.size === 'xs' ? '5px 0px' : '10px 40px'};
-    ${props => props.size === 'xs' ? 'font-size: 12px' : null};
-    border-radius: 5px;
-    font-weight: bold;
-    transition: all 0.2s ease-in-out;
-    text-align: center;
-    color: #2A9D8F;
-    cursor: pointer;
+const useStyles = makeStyles(theme =>
+  createStyles({
+    margin: {
+      margin: theme.spacing(1),
+    },
+  }),
+);
 
-    &:hover {
-        background-color: #2A9D8F;
-        color: white;
+const test = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2A9D8F'
     }
-`
+  }
+});
 
-const Button = (props) => {
+function StyledButton(props) {
+  const classes = useStyles();
+
   return (
-    <Wrapper {...props}>
-      {props.children}
-    </Wrapper>
+    <ThemeProvider theme={test}>
+      <Button variant="contained" className={classes.margin} onClick={props.onClick} {...props}>
+        {props.children}
+      </Button>
+    </ThemeProvider>
   )
 }
 
-export default Button;
+export default StyledButton

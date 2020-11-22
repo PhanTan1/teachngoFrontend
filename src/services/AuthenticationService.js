90b0e98cd9cbb2847693bd025908ae0f1cde3017
ffirouch <fouad.firouchane@gmail.com>
2020-11-22 20:28:16 +0900

    fix login

--- src/services/AuthenticationService.js
+++ src/services/AuthenticationService.js
@@ -2,10 +2,13 @@ import { client } from './Client'
 
 class AuthenticationService {
   async login(username, password) {
-    const response = await client.post(`login`).then(result => {
+    const response = await client.post(`login`,
+      { username, password }
+    )
+      .then(result => {
 
-      return result.data
-    })
+        return result.data
+      })
     return response
   }
 }

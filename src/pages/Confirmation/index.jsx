b02889a275591d50682e69e78c47949236fe876a
ffirouch <fouad.firouchane@gmail.com>
2020-11-23 01:55:12 +0900

    add skeleton to confirmation page

--- src/pages/Confirmation/index.jsx
+++ src/pages/Confirmation/index.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import LoadingIndicator from '../../components/LoadingIndicator'
+import Button from "../../components/Button"
+import AuthenticationService from "../../services/AuthenticationService"
+import { Redirect } from "react-router-dom"
+
+
+const Container = styled.div`
+  position: relative;
+  width: 100vw;
+  min-height: calc(100vh - 142px);
+  overflow: scroll;
+  padding: 20px;
+`
+
+const LoadingWrapper = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    z-index: 10001;
+    background-color: rgba(0, 0, 0, 0.8);
+`
 
 export default function Confirmation(props) {
+  const [registered, setRegistered] = useState(false)
+  const [redirect, setRedirect] = useState(false)
+  const { token, login } = props.match.params
+
+  useEffect(() => {
+    AuthenticationService.activateAccount(token, login)
+      .then(() => {
+        setRegistered(true)
+        setTimeout(() => {
+          setRedirect(true)
+        }, 3000)
+      })
+  }, [])
+
   return (
-    <div>
-      Confirmed
-      <div>{props.match.params.token}</div>
-      <div>{props.match.params.login}</div>
-    </div>
+    <Container>
+      {registered ?
+        redirect ?
+          <Redirect to="/login" />
+          :
+          <div>
+            Votre compte a ete active, vous allez etre rediriger vers la page de connexion.
+        </div>
+        :
+        <LoadingWrapper>
+          <LoadingIndicator />
+        </LoadingWrapper>
+
+      }
+    </Container>
   )
 }

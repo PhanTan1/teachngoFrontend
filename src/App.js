861c2b1db3bf535672d1bfb4e57e4338760a583c
ffirouch <fouad.firouchane@gmail.com>
2020-11-23 21:18:50 +0900

    refactored routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React, { useState } from 'react'
 import './App.css';
 import styled from "styled-components"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 import Teachers from "./pages/Teachers"
 import Students from "./pages/Students"
 import Home from "./pages/Home"
@@ -19,22 +20,53 @@ const Wrapper = styled.div`
 `
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
+  const [role, setRole] = useState(0)
+  const [id, setId] = useState('')
+
+  function PrivateRoute({ children, ...rest }) {
+    return (
+      <Route
+        {...rest}
+        render={({ location }) =>
+          isLoggedIn ? (
+            children
+          ) : (
+              <Redirect
+                to={{
+                  pathname: "/login",
+                  state: { from: location }
+                }}
+              />
+            )
+        }
+      />
+    );
+  }
+
   return (
     <div className="App">
       <Router>
-        <Navigation />
+        <Navigation role={role} isLoggedIn={isLoggedIn} id={id} />
         <Switch>
           <Wrapper>
+            <PrivateRoute path="/teachers/:id/reservation">
+              <Reservation />
+            </PrivateRoute>
+            <PrivateRoute path="/teacher/:id/profile" >
+              <PersonalProfile />
+            </PrivateRoute>
+            <PrivateRoute path="/students" >
+              <PersonalProfile />
+              <Students />
+            </PrivateRoute>
             <Route path="/confirmation/:token/:login" exact render={props => <Confirmation {...props} />} />
-            <Route path="/teachers/:id/reservation" exact render={props => <Reservation {...props} />} />
-            <Route path="/teachers/:id/profile" exact render={props => <PersonalProfile {...props} />} />
-            <Route path="/students/:id/reservation" exact render={props => <Reservation {...props} />} />
             <Route path="/teachers/:id" exact render={props => <ProfilTeacher {...props} />} />
             <Route path="/students/:id" exact render={props => <ProfilStudent {...props} />} />
-            <Route path="/login" exact render={props => <Login {...props} />} />
+            <Route path="/login" exact render={props => <Login setIsLoggedIn={setIsLoggedIn} setRole={setRole} {...props} />} />
             <Route path="/signup" exact render={props => <Signup {...props} />} />
             <Route path="/teachers/list/:category" exact render={props => <Teachers {...props} />} />
-            <Route path="/students" exact render={props => <Students {...props} />} />
+
             <Route path="/" exact render={props => <Home {...props} />} />
           </Wrapper>
         </Switch>
